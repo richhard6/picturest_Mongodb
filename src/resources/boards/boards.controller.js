14ab93005a4af1ab3821f0ff669db3ad0b5fbff7
Eudald <eaarranz@gmail.com>
2020-12-08 10:34:38 +0100

    feat: added the relations extra score exercise relative

--- src/resources/boards/boards.controller.js
+++ src/resources/boards/boards.controller.js
@@ -1,9 +1,33 @@
 const persimon = require('../../utils/persimon');
 const db = persimon('/assets/boards.json'); // Relative to the project root
+// 💯 Boards with pin counter: Add the pins DB also here
+const pinsDb = persimon('/assets/pins.json'); // Relative to the project root
+
+// 💯 Boards with pin counter: We need to define the logic that adds the referenced
+// pins to each board and counts them.
+// 💯 Boards with first 3 images: We need to define the logic that adds the first
+// 3 referenced pins to each board.
+const addReferencedPins = (boards) => {
+  const pins = pinsDb.all();
+  // 💯 Boards with pin counter: We iterate all the boards
+  const boardsWithPinsReferences = boards.map((board) => {
+    // 💯 Boards with pin counter: For each we search the pins with the board id
+    const pinsOfTheBoard = pins.filter((pin) => pin.board === board.id);
+    return {
+      ...board,
+      // 💯 Boards with pin counter:
+      numberOfPins: pinsOfTheBoard.length,
+      // 💯 Boards with first 3 images:
+      firstPins: pinsOfTheBoard.slice(0, 3),
+    };
+  });
+  return boardsWithPinsReferences;
+};
 
 const getAll = (req, res) => {
   const boards = db.all();
-  return res.status(200).json(boards);
+  const boardsWithPinsReferences = addReferencedPins(boards);
+  return res.status(200).json(boardsWithPinsReferences);
 };
 
 const getOne = (req, res) => {
